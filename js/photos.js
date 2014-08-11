04d2eeafeee693e3e660627f412643002da35303
Joseph Sheedy <joseph.sheedy@gmail.com>
2014-08-11 14:11:11 -0700

    integrated lscache for flickr

--- js/photos.js
+++ js/photos.js
@@ -7,18 +7,27 @@ Flickr = {
     var apiKey = "99c539c105b4c5aa15b7d9d903cc7e9c";
     var extras = "description";
     var photosetId = "72157646317762624";
-
+    var cacheKey = 'flickrphotos';
     this.container = container;
 
-    $.getJSON("https://api.flickr.com/services/rest?method=flickr.photosets.getPhotos"+
-          "&api_key="+apiKey+
-          "&photoset_id="+photosetId+
-          "&format=json&nojsoncallback=1"+
-         "&extras="+extras
-      )
-      .done(function(data) {
-        self.render(data.photoset.photo);
-      });
+    var cached = lscache.get(cacheKey);
+
+    if (cached) {
+      console.log('cache hit');
+      self.render(cached.photoset.photo);
+    } else {
+      console.log('cache miss'); 
+      $.getJSON("https://api.flickr.com/services/rest?method=flickr.photosets.getPhotos"+
+            "&api_key="+apiKey+
+            "&photoset_id="+photosetId+
+            "&format=json&nojsoncallback=1"+
+           "&extras="+extras
+        )
+        .done(function(data) {
+          lscache.set(cacheKey, data, 60);
+          self.render(data.photoset.photo);
+        });
+    }
   },
 
   render: function(photos) {
