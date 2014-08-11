f46091c5e6a4861b764e9145d2ea3a98d8ca1be3
Joseph Sheedy <joseph.sheedy@gmail.com>
2014-08-11 14:01:24 -0700

    styled photos

--- js/photos-template.js
+++ js/photos-template.js
@@ -8,9 +8,9 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 function program1(depth0,data) {
   
   var buffer = "", stack1, helper;
-  buffer += "\n  <div class=\"entry\">\n    <p>"
+  buffer += "\n	  <div class=\"photo\">\n			<a data-title=\"TUBALUBA\" data-footer=\""
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.description)),stack1 == null || stack1 === false ? stack1 : stack1._content)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "</p>\n      <a data-toggle=\"lightbox\" data-type=\"image\" href=\"http://farm";
+    + "\" data-toggle=\"lightbox\" data-type=\"image\" href=\"http://farm";
   if (helper = helpers.farm) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.farm); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
@@ -26,7 +26,7 @@ function program1(depth0,data) {
   if (helper = helpers.secret) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.secret); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
-    + "_b.jpg\">\n        <img style=\"width: 100%\" src=\"http://farm";
+    + "_b.jpg\">\n		  	<img style=\"\" src=\"http://farm";
   if (helper = helpers.farm) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.farm); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
@@ -42,16 +42,17 @@ function program1(depth0,data) {
   if (helper = helpers.secret) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.secret); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
-    + "_m.jpg\">\n      </a>\n  </div>\n";
+    + "_m.jpg\">\n			</a>\n	  </div>\n	";
   return buffer;
   }
 
-  buffer += "\n<h2 class=\"box-heading\">Photos</h2>\n";
+  buffer += "\n<div class=\"photos\">\n	";
   options={hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data}
   if (helper = helpers.photos) { stack1 = helper.call(depth0, options); }
   else { helper = (depth0 && depth0.photos); stack1 = typeof helper === functionType ? helper.call(depth0, options) : helper; }
   if (!helpers.photos) { stack1 = blockHelperMissing.call(depth0, stack1, {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data}); }
   if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n</div>\n";
   return buffer;
   });
 })();
