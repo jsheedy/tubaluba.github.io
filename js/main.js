f235dade7e61ea508e408ebfaac51e4d78b17474
Joseph Sheedy <joseph.sheedy@gmail.com>
2014-08-11 19:38:31 -0700

    updated twitter widget (for safari), moved index JS to min.'s

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(function(){
+  var calendarContainer = $('.calendar');
+  Calendar.init(calendarContainer);
+
+  var photoContainer = $('#photos_container');
+  Flickr.init(photoContainer);
+
+  $('span.mailme').mailme();
+  $('#quotesCarousel').carousel();
+
+
+  $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+  }); 
+
+  var carousel = $('#myCarousel');
+  if (! carousel.is(':hidden')) {
+    $.each(carousel.find('img'), function(index, img) {
+      img.src = img.dataset.src;
+    });
+    carousel.carousel();
+
+    var feed = new Instafeed({
+      get: 'user',
+      userId: 363408176,
+      accessToken: '363408176.467ede5.4c61508b403943589d7ffa4535137baf',
+      limit: 10
+    });
+    feed.run();
+  }
+
+});
